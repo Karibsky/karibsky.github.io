6286d753cebd6e02527db6ecbf91226e4e6462df
Karibsky <defendmax@gmail.com>
2020-02-08 22:51:02 +0500

    completed redesign

--- js/main.js
+++ js/main.js
@@ -12,20 +12,23 @@ $.ajax({
     success: function(data) {
         var data = data['feed']['entry'];
         var out = '';
-        for (var i = 0; i < data.length; i++) {
-            out += `<div class="col-md-6" data-aos="fade-up" data-aos-anchor-placement="top-center" data-aos-duration="2000" data-aos-delay="100">`;
-            out += `<figure class="col-md-2 project">`;
-            out += `<img src="${data[i]['gsx$image']['$t']}" alt="project-image"/>`;
-            out += `<figcaption>`;
-            out += `<h2>${data[i]['gsx$name']['$t']}</h2>`;
-            out += `<p>${data[i]['gsx$description']['$t']}</p>`;
-            out += `<a href="${data[i]['gsx$url']['$t']}">Перейти</a>`;
-            out += `</figcaption>`;
-            out += `</figure>`;
-            out += `</div>`;
-        }
-        $(".services .row").html(out).ready(function(){
+        if(data.length > 0) {
+            $.each(data, function(key, value){
+                var flip = 'left';
+                if(key % 2 == 0)
+                    flip = 'right';
+                out += `<div class="col-md-4" data-aos="flip-${flip}" data-aos-anchor-placement="bottom" data-aos-duration="2000" data-aos-delay="500">`;
+                out += `<figure class="col-md-3 project">`;
+                out += `<img src="${value['gsx$image']['$t']}" alt="project-image"/>`;
+                out += `<figcaption>`;
+                out += `<h2>${value['gsx$name']['$t']}</h2>`;
+                out += `<p>${value['gsx$description']['$t']}</p>`;
+                out += `<a href="${value['gsx$url']['$t']}">Перейти</a>`;
+                out += `</figcaption>`;
+                out += `</figure>`;
+                out += `</div>`;
+            });
             $('.services .row').html(out);
-        });
+        }
     }
 });
