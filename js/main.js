7e247c67b2884cfbb589868648219b05a2028d8d
Karibsky <defendmax@gmail.com>
2020-05-11 00:43:58 +0500

    Refactoring animation

--- js/main.js
+++ js/main.js
@@ -1,34 +1,33 @@
-AOS.init();
+$(function() {
+    AOS.init({
+        useClassNames: false,
+        disableMutationObserver: false
+    });
 
-(function SetDate() {
-    var today = new Date();
-    document.getElementById('copyright').innerHTML = "Karibsky developer &copy; " + today.getFullYear();
-}());
+    $('#copyright').append(`Karibsky developer &copy; ${(new Date).getFullYear()}`);
 
-$.ajax({
-    url: 'https://spreadsheets.google.com/feeds/list/1gLyy0BKWt8ZbGb_9_Zozk600OUJmkMwDxHOh_leoYVU/od6/public/values?alt=json',
-    dataType: 'json',
-    async: true,
-    success: function(data) {
-        var data = data['feed']['entry'];
-        var out = '';
-        if(data.length > 0) {
-            $.each(data, function(key, value){
-                var flip = 'left';
-                if(key % 2 == 0)
-                    flip = 'right';
-                out += `<div class="col-md-4" data-aos="flip-${flip}" data-aos-anchor-placement="bottom" data-aos-duration="2000" data-aos-delay="500">`;
-                out += `<figure class="col-md-3 project">`;
-                out += `<img src="${value['gsx$image']['$t']}" alt="project-image"/>`;
-                out += `<figcaption>`;
-                out += `<h2>${value['gsx$name']['$t']}</h2>`;
-                out += `<p>${value['gsx$description']['$t']}</p>`;
-                out += `<a href="${value['gsx$url']['$t']}">Перейти</a>`;
-                out += `</figcaption>`;
-                out += `</figure>`;
-                out += `</div>`;
-            });
-            $('.services .row').html(out);
+    $.ajax({
+        url: 'https://spreadsheets.google.com/feeds/list/1gLyy0BKWt8ZbGb_9_Zozk600OUJmkMwDxHOh_leoYVU/od6/public/values?alt=json',
+        dataType: 'json',
+        async: true,
+        success: function(data) {
+            var data = data['feed']['entry'];
+            var out = '';
+            if(data.length > 0) {
+                $.each(data, function(key, value){
+                    out += `<div class="col-md-4" data-aos="flip-${key % 2 == 0 ? 'left' : 'right'}" data-aos-duration="2000" data-aos-delay="${key * 100}">`;
+                    out += `<figure class="col-md-3 project">`;
+                    out += `<img src="${value['gsx$image']['$t']}" alt="${value['gsx$name']['$t']} project image"/>`;
+                    out += `<figcaption>`;
+                    out += `<h2>${value['gsx$name']['$t']}</h2>`;
+                    out += `<p>${value['gsx$description']['$t']}</p>`;
+                    out += `<a href="${value['gsx$url']['$t']}">Перейти</a>`;
+                    out += `</figcaption>`;
+                    out += `</figure>`;
+                    out += `</div>`;
+                });
+                $('.services .row').html(out);
+            }
         }
-    }
+    });
 });
