99e3a08d1f629f6a8bb29bf751f54261ea2a3878
Karibsky <defendmax@gmail.com>
2020-11-11 17:34:32 +0500

    Сделал лейблы для используемых технологий в проекте. Немного отрефачил код. Добавил медиа запросы для планшетов

--- js/main.js
+++ js/main.js
@@ -37,11 +37,18 @@ $(function() {
         if (data.length > 0) {
             data.forEach(function(item, i){
                 out += `<div class="col-md-4" data-aos="flip-${i % 2 == 0 ? 'left' : 'right'}" data-aos-duration="2000" data-aos-delay="${i * 100}">`;
-                out += `<figure class="col-md-3 project">`;
+                out += `<figure class="col-md-4 project">`;
                 out += `<img src="${item['gsx$image']['$t']}" alt="${item['gsx$name']['$t']} project image"/>`;
                 out += `<figcaption>`;
+                item["gsx$labels"]["$t"].split(" ").forEach(function(item) {
+                    var labelColor = getLabelBackgroundByTechnologyName(item);
+                    out += `<span class="project_label" style="border-bottom: 1px solid ${labelColor};">${item}</span>`
+                });
                 out += `<h2>${item['gsx$name']['$t']}</h2>`;
                 out += `<p>${item['gsx$description']['$t']}</p>`;
+                out += `<p class="project_labels">`
+               
+                out += `</p>`;
                 out += `<a href="${item['gsx$url']['$t']}">Перейти</a>`;
                 out += `</figcaption>`;
                 out += `</figure>`;
@@ -50,4 +57,24 @@ $(function() {
             $('.services .row').html(out);
         }
     };
+
+    function getLabelBackgroundByTechnologyName(technologyName) {
+        switch(technologyName)
+        {
+            case 'scss':
+                return "#dba0c6";
+            case 'css':
+                return "#264de4";
+            case 'html':
+                return "#e34c26";
+            case 'js':
+                return "#f7e018";
+            case 'sql':
+                return "#0277bd";
+            case 'php':
+                return "#787CB5";
+            case 'c#':
+                return "#690080";
+        }
+    }
 });
